372fbb851a7cc9775a9ef2b8405a9aac687ca782
tanaphon <a.tanaphon@gmail.com>
2017-04-18 22:11:46 +0700

    test List immutable

--- voting-server/test/immutable_spec.js
+++ voting-server/test/immutable_spec.js
@@ -1,4 +1,5 @@
 import { expect } from 'chai'
+import { List } from 'immutable'
 
 describe('immutability', () => {
     describe('a number', () => {
@@ -12,6 +13,24 @@ describe('immutability', () => {
 
             expect(nextState).to.equal(43)
             expect(state).to.equal(42)
+        })
+    })
+
+    describe('A List', () => {
+        function addMovie(currentState, movie) {
+            return currentState.push(movie)
+        }
+
+        it('is imutable', () => {
+            let state = List.of('Fast 8', 'Your Name')
+            let nextState = addMovie(state, '500 Days of Summer')
+
+            expect(nextState).to.equal(List.of(
+                'Fast 8', 'Your Name', '500 Days of Summer'
+            ))
+            expect(state).to.equal(List.of(
+                'Fast 8', 'Your Name'
+            ))
         })
     })
 })
