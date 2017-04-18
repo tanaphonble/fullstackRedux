9def765f1bccf9537466a4b0371b1f0306828c3e
tanaphon <a.tanaphon@gmail.com>
2017-04-18 22:23:53 +0700

    test Map immutable

--- voting-server/test/immutable_spec.js
+++ voting-server/test/immutable_spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { List } from 'immutable'
+import { List, Map } from 'immutable'
 
 describe('immutability', () => {
     describe('a number', () => {
@@ -22,15 +22,42 @@ describe('immutability', () => {
         }
 
         it('is imutable', () => {
-            let state = ['Fast 8', 'Your Name']
+            let state = List.of('Fast 8', 'Your Name')
             let nextState = addMovie(state, '500 Days of Summer')
 
-            expect(nextState).to.equal([
+            expect(nextState).to.equal(List.of(
                 'Fast 8', 'Your Name', '500 Days of Summer'
-            ])
-            expect(state).to.equal([
+            ))
+            expect(state).to.equal(List.of(
                 'Fast 8', 'Your Name'
-            ])
+            ))
+        })
+    })
+
+    describe('a tree', () => {
+        function addMovie(currentState, movie) {
+            return currentState.set(
+                'movies',
+                currentState.get('movies').push(movie)
+            )
+        }
+
+        it('is immutable', () => {
+            let state = Map({
+                movies: List.of('Fast 8', 'Your Name')
+            })
+            let nextState = addMovie(state, '500 Days of Summer')
+
+            expect(nextState).to.equal(Map({
+                movies: List.of(
+                    'Fast 8', 'Your Name', '500 Days of Summer'
+                )
+            }))
+            expect(state).to.equal(Map({
+                movies: List.of(
+                    'Fast 8', 'Your Name'
+                )
+            }))
         })
     })
 })
