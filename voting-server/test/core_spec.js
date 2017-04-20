5db4412f0d0dc94bf42fbf9fdde2b20fb595ab64
tanaphon <a.tanaphon@gmail.com>
2017-04-20 11:12:25 +0700

    converts to immutable

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -5,9 +5,9 @@ import { setEntries } from '../src/core'
 
 describe('application logic', () => {
     describe('setEntries', () => {
-        it('adds the entries to the state', () => {
+        it('converts to immutable', () => {
             const state = Map()
-            const entries = List.of('Fast 8', 'Your Name')
+            const entries = ['Fast 8', 'Your Name']
             const nextState = setEntries(state, entries)
             expect(nextState).to.equal(Map({
                 entries: List.of('Fast 8', 'Your Name')
